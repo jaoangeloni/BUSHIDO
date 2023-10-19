bbdbf8df82f0c4bbd0530909ac0e20ce9a955637
Joao Angeloni <joaopedroangeloni@gmail.com>
2023-10-19 09:49:14 -0300

    commit

---
+++ script.js
@@ -0,0 +1,75 @@
+function hover1() {
+    const description = document.getElementById('description1');
+    description.innerHTML = 'Na gestão de projetos, a honestidade é fundamental. Os líderes devem ser justos em suas avaliações e decisões, promovendo um ambiente de trabalho transparente e ético.'
+}
+
+function nothover1() {
+    const description = document.getElementById('description1');
+    description.innerHTML = 'Esta regra diz respeito à integridade. Você deve acreditar que a justiça não é algo que é praticado pelos outros, mas algo que você respeita todos os dias. Deve ser honesto sempre e tomar suas decisões baseadas naquilo que é certo e justo para as pessoas ao seu redor.'
+}
+
+
+function hover2() {
+    const description = document.getElementById('description2');
+    description.innerHTML = 'A coragem é vital para lidar com riscos e incertezas em um projeto. Os gerentes de projetos devem ter a coragem de tomar decisões difíceis e enfrentar desafios com determinação.'
+}
+
+function nothover2() {
+    const description = document.getElementById('description2');
+    description.innerHTML = 'Viver é arriscado e perigoso. Ficar escondido não é a melhor maneira de encarar a vida. Você deve se esforçar para viver ao máximo e intensamente. Mas ser corajoso não é ser um idiota. Você deve ter inteligência e cautela por trás de seus atos.'
+}
+
+
+function hover3() {
+    const description = document.getElementById('description3');
+    description.innerHTML = 'A benevolência na gestão de projetos se manifesta na preocupação com o bem-estar da equipe. Os gerentes devem se importar com o desenvolvimento e o sucesso de seus membros, incentivando um ambiente de trabalho positivo.'
+}
+
+function nothover3() {
+    const description = document.getElementById('description3');
+    description.innerHTML = 'Um samurai treina arduamente para fazer o bem. Amor, amizade, solidariedade e nobreza de sentimentos são considerados como os maiores atributos da alma. Ajude seus colegas em todas as oportunidades que houver.'
+}
+
+
+function hover4() {
+    const description = document.getElementById('description4');
+    description.innerHTML = 'O respeito é essencial para criar uma cultura de colaboração e comunicação aberta. Os gerentes devem respeitar as opiniões e contribuições de todos os membros da equipe, independentemente de seu cargo ou posição.'
+}
+
+function nothover4() {
+    const description = document.getElementById('description4');
+    description.innerHTML = 'O samurai não tem nenhuma razão para ser cruel. Não há necessidade de provar a sua força. Ele deve ser cortês até mesmo para com os seus inimigos. Se não fosse assim, ele não seria melhor do que qualquer animal. Um homem é respeitado não só por sua coragem, mas também pela forma como trata os outros os seu redor.'
+}
+
+
+function hover5() {
+    const description = document.getElementById('description5');
+    description.innerHTML = 'A honestidade absoluta é crucial na gestão de projetos, especialmente ao lidar com clientes e partes interessadas. A transparência em relação ao progresso do projeto e aos desafios enfrentados ajuda a construir confiança e credibilidade.'
+}
+
+function nothover5() {
+    const description = document.getElementById('description5');
+    description.innerHTML = 'Mentir é um ato covarde e desonroso. Quando um samurai diz que vai fazer algo, é como se já tivesse feito. Nada vai impedi-lo de cumprir uma promessa.'
+}
+
+
+function hover6() {
+    const description = document.getElementById('description6');
+    description.innerHTML = 'A honra no contexto da gestão de projetos pode ser vista como o compromisso de entregar resultados de alta qualidade dentro dos prazos e orçamentos acordados. Manter a palavra e cumprir promessas é fundamental para construir uma reputação sólida.'
+}
+
+function nothover6() {
+    const description = document.getElementById('description6');
+    description.innerHTML = 'O verdadeiro samurai só tem um juiz de sua honra: ele mesmo. As escolhas que você faz e como você trabalha para obtê-las são um reflexo de quem você realmente é. Você não pode se esconder de si mesmo.'
+}
+
+
+function hover7() {
+    const description = document.getElementById('description7');
+    description.innerHTML = 'A lealdade na gestão de projetos implica o compromisso com os objetivos do projeto e com a equipe. Os gerentes devem demonstrar lealdade ao cliente, à organização e aos membros da equipe, trabalhando para o sucesso coletivo e não apenas para interesses individuais.'
+}
+
+function nothover7() {
+    const description = document.getElementById('description7');
+    description.innerHTML = 'Um samurai é leal àqueles que estão sob seus cuidados. Por quem ele é responsável, ele permanece fiel.'
+}
